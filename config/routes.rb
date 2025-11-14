4f766f0ffff4a8fb02b867501b89226fffdfb71e
Tasuku Koni <koni.tasuku@gmail.com>
2025-11-14 11:02:11 +0900

    added any leftovers from yesterday

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :lists, only:[:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
 
+  end
+
+  resources :bookmarks, only:[:destroy]
+  root to: 'lists#index'
   # Defines the root path route ("/")
   # root "posts#index"
 end
