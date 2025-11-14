2bc8a34a7189c6cc3db87c98debb62af2620a5c7
Tasuku Koni <koni.tasuku@gmail.com>
2025-11-14 12:21:54 +0900

    updating the figure as of 12pm

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -23,7 +24,7 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 
 end
